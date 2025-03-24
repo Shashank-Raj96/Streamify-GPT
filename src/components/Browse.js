82bfb30401acfc20421507e4c33cd43e19e80e87
Shashank-Raj96 <shashankraj0915@gmail.com>
2025-03-25 00:40:01 +0530

    creating an new Hook for the movie

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,23 +1,11 @@
 
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
+import useNowPlayingMovies from "../hook/useNowPlayingMovies";
 import Header from "./Header";
 
 const Browse = () => {
 
-    const getNowPlayingMovies = async () => {
-        const data = await fetch(
-            'https://api.themoviedb.org/3/movie/now_playing?page=1',
-            API_OPTIONS
-        ); 
-        const json = await data.json();
-        console.log(json.results);
-    }; 
-
-    useEffect(() => {
-        getNowPlayingMovies();
-    }, []);
-
+    useNowPlayingMovies();
+   
     return (
         <div>
        <Header/>
