30f3d8ba93a8c8a0b0f066057fd8b65dc79f714c
Shashank-Raj96 <shashankraj0915@gmail.com>
2025-03-26 00:34:02 +0530

    Developing Movie Trailer (Autoplay )

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,40 +1,18 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/movieSlice";
 
-const VideoBackground = (movieId) => {
+import {  useSelector } from "react-redux";
+import useMovieTrailer from "../hook/useMovieTrailer";
+
+
+const VideoBackground = ({movieId}) => {
     const trailerVideo = useSelector(store => store.movies?.trailerVideo);
-  const dispatch = useDispatch();
+  
+    useMovieTrailer(movieId);
 
-
-    // fetch trailer video for this we need movie id 
-    const getMovieVideos = async () => {
-        const data = await fetch(
-        'https://api.themoviedb.org/3/movie/822119/videos?language=en-US',
-         API_OPTIONS
-        );
-   
-        const json = await data.json();
-        console.log(json.results);
-
-        const filterData = json.results.filter(video => video.type === "Trailer");
-        const trailer =  filterData.length ? filterData[0] : json.results[0];
-        console.log(trailer);
-        dispatch(addTrailerVideo(trailer));
-        
-    };
-
-    useEffect(() => {
-        getMovieVideos();
-    } , [] );
-    
     return (
-        <div>
+        <div className = "w-screen ">
             <iframe
-             width="560"
-              height="315"
-               src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+             className="w-screen aspect-video"
+               src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?autoplay=1&mute=1"}
                 title="YouTube video player"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                   referrerPolicy="strict-origin-when-cross-origin"
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,16 +1,16 @@
 const VideoBackTitle = ({title , overview}) => {
     return (
-        <div className="pt-24 px-14">
+        <div className="w-scree aspect-video pt-[17%] px-24 absolute text-white bg-gradient-to-r from-black">
             <h1 className="text-4xl font-bold">{title}</h1>
-            <p className="py-6 text-lg w-1/4">{overview}</p>
+            <p className="py-6 text-lg w-1/3">{overview}</p>
 
             <div className="">
-                <button className=" bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg">
+                <button className=" bg-white text-black p-4 px-12 text-xl  rounded-lg hover:bg-opacity-70">
                    ▶️ Play 
                 </button>
 
-                <button  className="mx-2 bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg">
-                    More Info
+                <button  className="mx-2 bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg hover:bg-opacity-70">
+                  ℹ️ More Info
                 </button>
 
             </div>
